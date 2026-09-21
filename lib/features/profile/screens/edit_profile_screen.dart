b7bf7fe5706b40f02b940d863530736cd64463c8
Noman Amir <nomanamir664@gmail.com>
2026-09-21 08:14:49 +0500

    feat: implement user profile with photo upload and editable personal details

--- lib/features/profile/screens/edit_profile_screen.dart
+++ lib/features/profile/screens/edit_profile_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
-import '../../../core/theme/app_colors.dart';
+//import '../../../core/theme/app_colors.dart';
 import '../../../shared/widgets/app_text_field.dart';
 import '../controllers/profile_controller.dart';
 
